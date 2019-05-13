45f93c330fb870b97a793f9257e39e9c01278f8a
Errorcod3 <mantus@live.com>
2019-05-13 16:28:19 -0500

    updates port pipe

--- server/config.js
+++ server/config.js
@@ -3,3 +3,5 @@ process.env.database = "d6cdlvhs2okrtu"
 process.env.user = "ytmjoqifpqimft"
 process.env.port = "5432"
 process.env.password = "c4b6096224811258823659d760905018b3836c19f0450985372ceeb533aa93bd"
+
+// postgres://ytmjoqifpqimft:c4b6096224811258823659d760905018b3836c19f0450985372ceeb533aa93bd@ec2-23-23-92-204.compute-1.amazonaws.com:5432/d6cdlvhs2okrtu
